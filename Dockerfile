b943beb2bb0da168421bd96de5f3644fe554a848
kimjeffsj <165725029+kimjeffsj@users.noreply.github.com>
2025-03-26 16:47:08 -0700

    feat: Dockerfile and database updated

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,15 @@ COPY . .
 # 빌드
 RUN npm run build
 
-# 실행
+# 실행 (스크립트 파일 생성)
+RUN echo '#!/bin/sh \n\
+  if [ "$NODE_ENV" != "production" ]; then \n\
+  echo "Running in development mode, using synchronize" \n\
+  elif [ "$RUN_MIGRATIONS" = "true" ]; then \n\
+  echo "Running migrations..." \n\
+  npm run migration:run \n\
+  fi \n\
+  node dist/app.js' > /app/start.sh && chmod +x /app/start.sh
+
 EXPOSE 5001
-CMD ["node", "dist/app.js"]
+CMD ["/app/start.sh"]
--- src/config/database.ts
+++ src/config/database.ts
@@ -6,16 +6,21 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host:
+    process.env.DB_HOST ||
+    "starclonedb.c1uemwswgazh.us-west-2.rds.amazonaws.com",
   port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_DATABASE || "instagram_clone",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: process.env.NODE_ENV !== "production",
   logging: process.env.NODE_ENV !== "production",
   entities: [join(__dirname, "../entities/**/*.{ts,js}")],
   migrations: [join(__dirname, "../migrations/**/*.{ts,js}")],
   subscribers: [join(__dirname, "../subscribers/**/*.{ts,js}")],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 /**
