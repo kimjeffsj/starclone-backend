423b32cfd93be3f4b9bcf029d0fdb6f0d29e0a7d
kimjeffsj <165725029+kimjeffsj@users.noreply.github.com>
2025-03-26 22:43:11 -0700

    refactor: database setup

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,9 +6,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host:
-    process.env.DB_HOST ||
-    "starclonedb.c1uemwswgazh.us-west-2.rds.amazonaws.com",
+  host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
